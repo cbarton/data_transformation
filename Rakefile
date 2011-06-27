0d387faf10feb3cb2f2ed6099f3cfba14693618b
Chris Barton <cbarton@inigral.com>
2011-06-27 02:19:32 -0700

    loading tasks

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 require 'bundler/gem_tasks'
 
 require 'rspec/core/rake_task'
-require 'data_transformations/tasks'
+require 'data_transformation/tasks'
 
 RSpec::Core::RakeTask.new(:spec)
--- lib/data_transformation.rb
+++ lib/data_transformation.rb
@@ -2,5 +2,9 @@ require "data_transformation/version"
 require 'data_transformation/transformer'
 
 module DataTransformation
-  # Your code goes here...
+	class Railtie < Rails::Railtie
+		rake_tasks do
+			load 'lib/tasks/data_transformations.rake'
+		end
+	end
 end
