2443ec4500b1e5298e6b15e1740a3b16a46534ac
Chris Barton <cbarton@inigral.com>
2011-06-30 14:27:12 -0700

    pathing

--- lib/data_transformation/transformer.rb
+++ lib/data_transformation/transformer.rb
@@ -5,12 +5,18 @@ module DataTransformation
 		class << self
 			def transform(transforms_path, target_version=nil)
 				migrate(transforms_path, target_version)
-			end			
+			end	
+
+			def migrations_paths
+				@migrations_paths ||= ['db/transforms']
+				Array.wrap(@migrations_paths)
+			end		
+			
+			def migrations_path
+				migrations_paths.first
+			end
 		end
 
-		def migrations_path
-			'db/transforms'
-		end
 
 		def schema_migrations_table_name
 			ActiveRecord::Base.table_name_prefix + "schema_transforms" + ActiveRecord::Base.table_name_suffix
